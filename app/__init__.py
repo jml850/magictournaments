61b9d31c720b1ba050d0d47fc13d4094aebeacf7
jml850 <122303975+jml850@users.noreply.github.com>
2023-12-08 13:41:50 +0100

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import SECRET_KEY
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "auth.login"
+
+
+# Importa los blueprints definidos en los archivos routes
+from app.routes.auth import auth as auth_blueprint
+from app.routes.main import main as main_blueprint
+
+# Registra los blueprints con la aplicación
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(main_blueprint)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User  # Ahora, importa User dentro de esta función
+    return User.query.get(int(user_id))
+
+
---
+++ app/forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[DataRequired(), Length(min=1, max=20)])
+    password = PasswordField(validators=[DataRequired(), Length(min=1, max=20)])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField()
+
+class LeagueForm(FlaskForm):
+    league_name = StringField(validators=[DataRequired()])
+    player1 = StringField(validators=[DataRequired()])
+    player2 = StringField(validators=[DataRequired()])
+    player3 = StringField()
+    player4 = StringField()
+    deck1 = StringField(validators=[DataRequired()])
+    deck2 = StringField(validators=[DataRequired()])
+    deck3 = StringField()
+    deck4 = StringField()
+    deck5 = StringField()
+    deck6 = StringField()
+    deck7 = StringField()
+    deck8 = StringField()
+    submit = SubmitField()
+
+class TournamentForm(FlaskForm):
+    tournament_name = StringField(validators=[DataRequired()])
+    players = StringField()
+    decks = StringField()
+    submit = SubmitField()
+
+class MatchForm(FlaskForm):
+    selectdecks = StringField()
+    players = StringField()
+    winner = StringField()
+    submit = SubmitField()
+
+class EliminateForm(FlaskForm):
+    leagues = StringField()
+    tournaments = StringField()
+    submit = SubmitField()
+
+class ChangeForm(FlaskForm):
+    username = StringField(validators=[DataRequired(), Length(min=1, max=20)])
+    password = PasswordField(validators=[DataRequired(), Length(min=1, max=20)])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')])
